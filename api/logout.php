8b5629260d6273d484762af7aa0a41174d3aeeb5
K4viyamato <kavinduhirushakarunarathna@gmail.com>
2023-11-09 21:18:05 +0530

    faaaaaak

--- api/logout.php
+++ api/logout.php
@@ -3,8 +3,11 @@
 
     if (isset($_SESSION['username'])){
         // logged in
-        header('Location: /index.php');
         session_destroy();
+
+        header('Location: /index.php');
+        exit;
+        // echo "<script>location.href='index.php'</script>";
     } 
     
 
