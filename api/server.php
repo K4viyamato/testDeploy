2336d8e97bb2e4e4a34048a10fea341ea35ce0c2
K4viyamato <kavinduhirushakarunarathna@gmail.com>
2023-11-09 20:55:37 +0530

    editing the serverphp

--- api/server.php
+++ api/server.php
@@ -8,16 +8,22 @@
     $password = "1234";
 
     if ( $_POST['name'] == $username && $_POST['pwd'] == $password){
-        echo "You logged in";
+        
         $_SESSION['username'] = $_POST['name'];
+        
+        if(isset($_POST['remember'])){
+            setCookie("username", $_POST['name'], time()+86400);
+            setCookie("password",  $_POST['pwd'], time()+86400);
+        }
+        
+        header('Location: /success.php');
+        exit;
+
+
     } else {
         die("incorrect password");
     }
     
-    if(isset($_POST['remember'])){
-        setCookie("username", $_POST['name'], time()+86400);
-        setCookie("password",  $_POST['pwd'], time()+86400);
-    }
 
     echo "<br><a href='logout.php'>Logout</a><br>";
 
