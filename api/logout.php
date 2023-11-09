6e4e2d479ac26e5055327d1beaabb427d1f2993d
K4viyamato <kavinduhirushakarunarathna@gmail.com>
2023-11-09 21:06:44 +0530

    did it

--- api/logout.php
+++ api/logout.php
@@ -4,7 +4,7 @@
     if (isset($_SESSION['username'])){
         // logged in
         session_destroy();
-        echo "<script>location.href='index.php'</script>";
+        header('Location: /index.php');
     } 
     
 
